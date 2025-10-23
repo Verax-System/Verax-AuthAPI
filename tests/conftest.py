68eac9d005973226976fdf390ef7dff924cad28a
Vitorhleme <vitorhugolsenai6@gmail.com>
2025-10-23 19:52:23 -0300

    Reformat

--- tests/conftest.py
+++ tests/conftest.py
@@ -60,7 +60,9 @@ async def async_client(db_session: AsyncSession) -> AsyncGenerator[AsyncClient, 
 
     app.dependency_overrides[get_db] = override_get_db
 
-    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as client:
-     yield client
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as client:
+        yield client
 
     app.dependency_overrides.pop(get_db, None)
